b1368cd28daeb6862c1fd243a0bec67ff0b7d83c
Sahil Choudhary <115393634+sc941432@users.noreply.github.com>
2023-02-05 09:58:58 +0530

    Add files via upload

--- Employee management system.cpp
+++ Employee management system.cpp
@@ -1,8 +1,9 @@
-/* basic implement of cpp
-author Sahil Choudhary
+/* basic implement of c++ 
+author sahil choudhary
 roll no 104032
 div 4
-date 23/01/23 */
+date 31/01/2023
+*/
 
 #include<iostream>
 #include<string>
@@ -11,77 +12,106 @@ using namespace std;
 class employee
 {
     private:
-        string firstname;
-        string lastname;
-        string city;
-        int employeeid;
-        float DA,TA,base,HRA,netsalary;
-
+    string firstname;
+    string lastname;
+    string city;
+    int base;
+    float TA,DA,HRA,netsal;
+    static int count;
+   
+  
     public:
-         void getdata();
-         void showdata();
-         void calsalary();
-};
-int j=1;
-void employee::getdata()
-{
+    static int empid;
+    void getdata();
+    void calsal();
+    void showdata();
+    friend void promote(float netsalary);
     
-    cout<<"enter "<<j<<" employee name: "<<endl;
-    cin>>firstname;
-
-    cout<<"enter employee last name: "<<endl;
-    cin>>lastname;
-
-    cout<<"enter "<<j<<" employee id: "<<endl;
-    cin>>employeeid;
-
-    cout<<"enter "<<j<<" employee's city "<<endl;
-    cin>>city;
-
-    cout<<"enter your basic salary: "<<endl;
-    cin>>base;
-    j++;
+};
+void promote(float netsalary)
+{
+    float bouns;
+    if(netsalary>100000)
+    {
+        cout<<" you got bouns promotion "<<endl;
+        bouns=0.10*netsalary;
+        cout<<" you got "<<bouns<<" bouns rupees "<<endl;
+    }
+    else
+    cout<<" sorry you don't got bouns "<<endl;
 }
 
-void employee::calsalary()
+int employee::count=1;
+int employee::empid=104032;
+
+void employee::getdata()
+{
+   cout<<" employee id "<<empid<<endl;
+   empid++;
+
+    cout<<"enter "<<count<<" employee first name; "<<endl;
+    cin>>firstname;
+
+    cout<<"enter last name: "<<endl;
+    cin>>lastname;
+
+    cout<<"enter "<<count<<" employee city: "<<endl;
+    cin>>city;
+
+    cout<<"enter employee basic salary: "<<endl;
+    cin>>base;
+    
+    count++;
+}
+
+void employee::calsal()
 {
     TA=0.2*base;
-    DA=0.35*base;
-    HRA=0.25*base;
-    netsalary=base+TA+DA+HRA; 
+     DA=0.35*base;
+     HRA=0.25*base;
+    netsal=TA+DA+HRA+base;
 }
 
 void employee::showdata()
 {
+    
     cout<<endl;
-    cout<<"employee details given below: "<<endl;
-    cout<<"Name of employee: "<<firstname<<" "<<lastname<<endl;
-    cout<<"Employee ID: "<<employeeid<<endl;
-    cout<<"city: "<<city<<endl;
+    cout<<"Name: "<<firstname<<" "<<lastname<<endl;
+    cout<<"employee id: "<<empid<<endl;
+    cout<<"employee city: "<<city<<endl;
     cout<<"basic salary: "<<base<<endl;
-    cout<<"net salary : "<<netsalary<<endl;
+    cout<<"net salary: "<<netsal<<endl;
+    cout<<endl;
+    empid++;
+    promote(netsal);
+
 }
 
 int main()
 {
+
+    cout<<"*********************"<<endl;
+    cout<<"Apple Company Employee Information: "<<endl;
+    cout<<"*********************"<<endl;
     int n;
-    cout<<"****************************************"<<endl;
-    cout<<"Microsoft Tech company employee list: "<<endl;
-
-    cout<<"Enter number of employee in company: "<<endl;
+    cout<<"enter number of employees in company: "<<endl;
     cin>>n;
-    employee employ[n];
+    cout<<"enter "<<n<<" employee information: "<<endl;
+    employee employ[n]; //object employ array for employee class
 
     for(int i=0;i<n;i++)
     {
-        employ[i].getdata();
-        employ[i].calsalary();       
+        cout<<endl;
+         employ[i].getdata(); 
+       employ[i].calsal(); 
+           
     }
-
+    employee::empid=employee::empid-n;
     for(int i=0;i<n;i++)
     {
+        cout<<"Employee "<<i+1<<" information "<<endl;
         employ[i].showdata();
+        cout<<endl;
     }
-        
     return 0;
 }
